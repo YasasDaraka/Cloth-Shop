7f9d64083199cba3f9758a2f19cb9fb86d0b702d
YasasDaraka <darakayasas@gmail.com>
2024-06-11 21:48:40 +0530

    frontend Update

--- Frontend/frontend/src/view/pages/Admin/AdminPage.tsx
+++ Frontend/frontend/src/view/pages/Admin/AdminPage.tsx
@@ -1,12 +1,14 @@
 import {SideBar} from "../../common/NavBar/SideBar";
 import React from "react";
 import {NavBar} from "../../common/NavBar/NavBar";
+import {MainForm} from "../Form/MainForm";
 
 export function AdminPage() {
     return (
         <>
             <NavBar signIn={false} signUp={false} date={false} logOut={true}/>
             <SideBar/>
+            <MainForm/>
         </>
     );
 }
---
+++ Frontend/frontend/src/view/pages/Form/MainForm.tsx
@@ -0,0 +1,11 @@
+import {CommonForm} from "./CommonForm";
+
+export function MainForm() {
+    return (
+        <>
+            <div className="h-[90.3vh] z-[5]" id="main">
+                <CommonForm/>
+            </div>
+        </>
+    );
+}
