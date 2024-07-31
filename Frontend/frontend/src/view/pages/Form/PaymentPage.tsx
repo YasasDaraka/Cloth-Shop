047048b76a85830b54cd48076c4b4e933680fc99
YasasDaraka <darakayasas@gmail.com>
2024-07-31 10:26:27 +0530

    update

--- Frontend/frontend/src/view/pages/Form/PaymentPage.tsx
+++ Frontend/frontend/src/view/pages/Form/PaymentPage.tsx
@@ -10,6 +10,7 @@ import Button from "@mui/material/Button";
 interface Props {
     formData: any
 }
+
 export function PaymentPage({formData}: Props) {
     const {register, handleSubmit, reset, watch, formState: {errors}, setValue} = useForm()
     const [resetForm, setResetForm] = useState(false);
@@ -21,7 +22,7 @@ export function PaymentPage({formData}: Props) {
     const [balance, setBalance] = useState()
 
 
-    const handleChange = (event: { target: { name: any , value: any; }; }) => {
+    const handleChange = (event: { target: { name: any, value: any; }; }) => {
         setResetForm(false);
         const target = event.target;
         let name = target.name;
@@ -40,44 +41,44 @@ export function PaymentPage({formData}: Props) {
         <>
             <div className="flex justify-center align-items-center h-full w-full">
 
-                <div className="w-1/2 h-2/3 bg-amber-800">
+                <div className="w-1/2 h-2/3 ">
 
-                        {formData.map((section:any, index: any) => (
-                            <div key={index}
-                                 className={`flex justify-between w-full px-3 h-[13vh]`}>
-                                {section.map((data: any) => (
-                                    <div key={data.id}
-                                         className={`z-50 ${(section.length === 1 || section.length === 1) ? 'w-[18vw]' : 'w-[13.2vw]'}`}>
-                                        <InputItem
-                                            key={data.id}
-                                            id={data.id}
-                                            title={data.title}
-                                            placeholder={data.placeholder}
-                                            type={data.type}
-                                            description={data.description}
-                                            selectList={data.selectList}
-                                            register={register}
-                                            watch={watch}
-                                            value={data.value}
-                                            isEdit={data.isEdit}
-                                            setValue={setValue}
-                                            onChange={data.onChange}
-                                            errors={errors}
-                                            isRequired={data.required}
-                                            resetForm={resetForm}
-                                            setResetForm={setResetForm}
+                    {formData.map((section: any, index: any) => (
+                        <div key={index}
+                             className={`flex justify-between w-full px-3 h-[13vh]`}>
+                            {section.map((data: any) => (
+                                <div key={data.id}
+                                     className={`z-50 ${(section.length === 1 || section.length === 1) ? 'w-[18vw]' : 'w-[13.2vw]'}`}>
+                                    <InputItem
+                                        key={data.id}
+                                        id={data.id}
+                                        title={data.title}
+                                        placeholder={data.placeholder}
+                                        type={data.type}
+                                        description={data.description}
+                                        selectList={data.selectList}
+                                        register={register}
+                                        watch={watch}
+                                        value={data.value}
+                                        isEdit={data.isEdit}
+                                        setValue={setValue}
+                                        onChange={data.onChange}
+                                        errors={errors}
+                                        isRequired={data.required}
+                                        resetForm={resetForm}
+                                        setResetForm={setResetForm}
 
-                                        />
-                                    </div>
-                                ))}
-                            </div>
-                        ))}
+                                    />
+                                </div>
+                            ))}
+                        </div>
+                    ))}
 
                 </div>
                 <div className="w-1/2 h-2/3">
 
                     <div className={"flex w-full px-3 h-[26vh]"}>
-                        <div className={"flex w-3/5 h-full"}>
+                        <div className={"flex w-1/2 h-full"}>
                             <div className="flex flex-col justify-center items-center w-1/2 h-full">
                                 <Label className="mb-4 font-semibold">Cash Payment</Label>
                                 <img className="w-[5.5vw]" src={cashImg}/>
@@ -87,10 +88,19 @@ export function PaymentPage({formData}: Props) {
                                 <img className="w-[5.5vw]" src={card}/>
                             </div>
                         </div>
-                        <div className={"flex w-2/5 h-full bg-amber-500"}></div>
+                        <div className={"flex flex-col w-1/2 h-full "}>
+                            <div className={"flex flex-col justify-center w-1/2 h-1/2 ml-5"}>
+                                <span className="text-[#3d98ef] text-sm font-semibold">Total : </span>
+                                <div><span className="text-[#3d98ef] text-3xl">0</span> Rs/=</div>
+                            </div>
+                            <div className={"flex flex-col justify-center w-1/2 h-1/2 ml-5"}>
+                                <span className="text-[#ef5350] text-sm font-semibold">SubTotal : </span>
+                                <div><span className="text-[#ef5350] text-3xl">0</span> Rs/=</div>
+                            </div>
+                        </div>
                     </div>
-                    <div className={"flex-row w-full px-3 h-[26vh] bg-amber-400"}>
-                        <div className={"flex items-center justify-evenly w-full h-2/5 bg-black"}>
+                    <div className={"flex-row w-full px-3 h-[26vh] "}>
+                        <div className={"flex items-center justify-evenly w-full h-2/5"}>
 
                             <Input
                                 type="text"
@@ -109,7 +119,7 @@ export function PaymentPage({formData}: Props) {
                                 defaultValue={initialValue}
                             />
                         </div>
-                        <div className={"flex items-center w-full h-2/5 bg-blue-400"}>
+                        <div className={"flex items-center w-full h-2/5 "}>
                             <Input
                                 type="text"
                                 name="cash"
@@ -120,9 +130,28 @@ export function PaymentPage({formData}: Props) {
                             />
                             <Button sx={{marginLeft: 4}} variant="contained" color="success"
                                     size="small" type="button"
-                                    onClick={()=> {}}>Purchase</Button>
+                                    onClick={() => {
+                                    }}>Purchase</Button>
                         </div>
-                        <div className={"flex w-full h-2/5 bg-black"}></div>
+                        <div className={"flex justify-start items-center w-full h-1/5"}>
+
+                            <Button sx={{marginLeft: 4}} variant="contained" color="primary"
+                                    size="small" type="button"
+                                    onClick={() => {
+                                    }}>Add Item</Button>
+                            <Button sx={{marginLeft: 4}} variant="contained" color="warning"
+                                    size="small" type="button"
+                                    onClick={() => {
+                                    }}>Clear</Button>
+                            <Button sx={{marginLeft: 4}} variant="contained" color="success"
+                                    size="small" type="button"
+                                    onClick={() => {
+                                    }}>Update</Button>
+                            <Button sx={{marginLeft: 4}} variant="contained" color="error"
+                                    size="small" type="button"
+                                    onClick={() => {
+                                    }}>Delete</Button>
+                        </div>
                     </div>
                 </div>
             </div>
